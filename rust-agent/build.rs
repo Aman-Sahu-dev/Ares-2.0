072c2cfafe1f10f2efd15d2fd8a05442b1750628
Aman Sahu <amansahu3369@gmail.com>
2026-06-20 23:59:13 +0530

    write buid.rs and learn some mechanics

--- rust-agent/build.rs
+++ rust-agent/build.rs
@@ -1,3 +1,8 @@
-fn main() {
-    tonic_build::configure();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .compile(
+            &["proto/compute.proto"],
+            &["proto"],
+        )?;
+    Ok(())
 }
--- rust-agent/src/main.rs
+++ rust-agent/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::server,Request,Response,Status};
+use compute::execution_engine_server::{ExecutionEngine,ExecutionEngineServer};
+use compute::{TaskRequest,TaskResponse,NodeStatus,Empty};
+
+pub mod compute{
+    tonic::include_proto!("compute");
+}
+pub struct AresWorker{}
+
+impl ExecutionEngine for AresWorker {
+    
 }
