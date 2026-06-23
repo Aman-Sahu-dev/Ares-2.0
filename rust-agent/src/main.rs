09fc05bd1c7fbef373d2f11ae2a32e66099279d6
Aman Sahu <amansahu3369@gmail.com>
2026-06-23 18:42:47 +0530

    rewrite files and fix the structures

---
+++ rust-agent/src/main.rs
@@ -0,0 +1,39 @@
+use std::{net::Shutdown, sync::Arc};
+use tokio::sync::{oneshot,Semaphore};
+use tracing_subscriber::fmt::format::FmtSpan;
+
+mod config;
+mod execution;
+mod server;
+mod services;
+
+use config::Config;
+use execution::ShellExexutor;
+use server::run_server;
+
+#[tokio::main]
+async fn main()-> Result<(),Box<dyn std::error::Error>>{
+    let config = config::from_env;
+
+    tracing_subscriber::fmt()
+    .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+    .with_span_events(FmtSpan::CLOSE)
+    .json()
+    .init();
+
+    std::panic::set_hook(Box::new(|info|{
+        tracing::error!(panic = %info,"service panicked");
+    }));
+    let concurrency = Arc::new(Semaphore::new(Config.max_concurrent_tasks));
+    let executor = Arc<dyn execution::TaskExecutor> = Arc::new(ShellExexutor::new(concurrency));
+    let (shutdown_tx,shutdown_rx) = oneshot::channel();
+    let server_handle = tokio::spawn(run_server(config.bind_addr,executor,shutdown_rx));
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("failed to install ctrl+c handler");
+        tracing::info!("shutdonw signal revieved ");
+        let _ = shutdown_tx.send(());
+    });
+    server_handle.await??;
+    tracing::info!("server shutdown complete");
+    Ok(())
+}
