3dc78a590451f36a88896450e96ad88a3f30eb88
Aman Sahu <amansahu3369@gmail.com>
2026-06-28 22:13:39 +0530

    complete controller

--- Control-plane/src/main/java/com/ares/controlplane/controller/TaskController.java
+++ Control-plane/src/main/java/com/ares/controlplane/controller/TaskController.java
@@ -1,4 +1,31 @@
 package com.ares.controlplane.controller;
 
+import com.ares.controlplane.dto.TaskRequestDto;
+import com.ares.controlplane.dto.TaskResponseDto;
+import com.ares.controlplane.service.TaskService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+    public static final Logger log = LoggerFactory.getLogger(TaskController.class);
+    public final TaskService taskService;
+
+    public TaskController (TaskService taskService){
+        this.taskService = taskService;
+    }
+    @PostMapping
+    public ResponseEntity<TaskResponseDto> submitTask(@Valid @RequestBody TaskRequestDto dto){
+    log.debug("task submission : runtime = {}, entrypoint = {}",dto.runtime(),dto.entryPoint());
+    TaskResponseDto response = taskService.dispatch(dto);
+    log.debug("task completed: exitCode = {}, executionTimeNs = {}",response.exitCode(),response.executionTimeNs());
+    return ResponseEntity.ok(response);
+    }
 }
--- Control-plane/src/main/java/com/ares/controlplane/service/TaskService.java
+++ Control-plane/src/main/java/com/ares/controlplane/service/TaskService.java
@@ -1,4 +1,11 @@
 package com.ares.controlplane.service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TaskService {
+    private static final Logger log = LoggerFactory.getLogger(TaskService.class);
+
+    private final 
 }
